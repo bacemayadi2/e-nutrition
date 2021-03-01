6778adacf4764c64cb853a8565991d12416fb9a7
bacemayadi2 <bacem.ayadi@esprit.tn>
2021-03-01 17:01:43 +0100

    test avec autre front

---
+++ e-nutrition-symfony/src/Controller/AlimentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlimentController extends AbstractController
+{
+    /**
+     * @Route("/ajouteraliment", name="aliment")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/aliment/ajouterAliement.html.twig', [
+            'controller_name' => 'AlimentController',
+        ]);
+    }
+}
