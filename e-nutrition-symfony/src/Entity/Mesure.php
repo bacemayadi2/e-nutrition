9cf4fc39988edea4cbd19e75ed198f58cf18cc83
sabrine dach <sabrine.dachraoui@esprit.tn>
2021-03-04 23:05:26 +0100

    Ajout Entités BlogPost & Mesure

---
+++ e-nutrition-symfony/src/Entity/Mesure.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MesureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MesureRepository::class)
+ */
+class Mesure
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Taille;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $poids;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateMesure;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTaille(): ?float
+    {
+        return $this->Taille;
+    }
+
+    public function setTaille(float $Taille): self
+    {
+        $this->Taille = $Taille;
+
+        return $this;
+    }
+
+    public function getPoids(): ?float
+    {
+        return $this->poids;
+    }
+
+    public function setPoids(float $poids): self
+    {
+        $this->poids = $poids;
+
+        return $this;
+    }
+
+    public function getDateMesure(): ?\DateTimeInterface
+    {
+        return $this->dateMesure;
+    }
+
+    public function setDateMesure(\DateTimeInterface $dateMesure): self
+    {
+        $this->dateMesure = $dateMesure;
+
+        return $this;
+    }
+
+    public function calculerIMC(){
+        return ($this->Taille * ($this->poids ** 2));
+    }
+
+    public function poidsIdeal(){
+        // 50 + (0.91 × [height in centimeters − 152.4])
+        return 50 + (0.91 * ($this->Taille * 100 - 152.4));
+    }
+}
