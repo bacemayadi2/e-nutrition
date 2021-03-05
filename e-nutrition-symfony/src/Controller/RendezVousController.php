e66e6de2679eef3f623675bf2c4a05c52f05a8af
PC <abdelhamid.atallah@esprit.tn>
2021-03-05 11:07:14 +0100

    CRUD RDV SUCC

--- e-nutrition-symfony/src/Controller/RendezVousController.php
+++ e-nutrition-symfony/src/Controller/RendezVousController.php
@@ -22,20 +22,28 @@ class RendezVousController extends AbstractController
         ]);
 
     }
+
     /**
-     * @Route("/rendez_vous/{id}", name="agenda")
+     * @param RendezVousRepository $repository
+     * @return Response
+     * @Route("AfficheR", name="aff")
+
      */
-    public function getRendezVous($id , RendezVousRepository $repository ){
+    public function AfficherRendezVous(RendezVousRepository $repository ){
 
         //$repo= $this->getDoctrine()->getRepository(RendezVous::class   );
-        $rendezVous=$repository->find($id);
+        $rendezVous=$repository->findAll();
 
-        return $this->render('rendez_vous/rendezVous.html.twig', [
+        return $this->render('rendez_vous/listeRDV.html.twig', [
             'controller_name' => 'RendezVousController',
             'rendezVous' => $rendezVous,
         ]);
     }
+
     /**
+     * @param $id
+     * @param RendezVousRepository $repository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      * @Route("/rendez_vous/del/{id}", name="delete")
      */
     public function Delete($id , RendezVousRepository $repository){
@@ -43,15 +51,15 @@ class RendezVousController extends AbstractController
         $em=$this->getDoctrine()->getManager();
         $em->remove($rendezVous);
         $em->flush();
-        return $this->redirectToRoute('rendez_vous');
+        return $this->redirectToRoute('aff');
     }
     /**
      * @Route("CreateRendezvous", name="rdv")
      */
     function CreateRendezVous(Request $request)
     {
-        $user = new RendezVous();
-        $form = $this->createForm(RendezVousType::class, $user);
+        $RendezVous = new RendezVous();
+        $form = $this->createForm(RendezVousType::class, $RendezVous);
         $form->add("Ajouter", SubmitType::class);
 
         $form->handleRequest($request);
@@ -59,10 +67,10 @@ class RendezVousController extends AbstractController
         if ($form->isSubmitted() && $form->isValid())
         {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
+            $em->persist($RendezVous);
             $em->flush();
-            dump($form);
-            //return $this->redirectToRoute('CreateUser');
+
+            return $this->redirectToRoute('aff');
         }
         return $this->render('rendez_vous/rendezVous.html.twig ' ,['form'=>$form->createView()]);
     }
--- e-nutrition-symfony/src/Controller/SuccessStoryController.php
+++ e-nutrition-symfony/src/Controller/SuccessStoryController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\RendezVous;
+use App\Entity\SuccessStory;
+use App\Form\RendezVousType;
+use App\Form\SuccessStoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +22,30 @@ class SuccessStoryController extends AbstractController
         return $this->render('success_story/afficherMedicament.html.twig', [
             'controller_name' => 'SuccessStoryController',
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @Route ("CreateSuccess", name="Succ")
+     */
+
+    function CreateSuccess(Request $request)
+    {
+        $Success = new SuccessStory();
+        $form = $this->createForm(SuccessStoryType::class, $Success);
+        $form->add("Ajouter", SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($Success);
+            $em->flush();
+
+            return $this->redirectToRoute('aff');
+        }
+        return $this->render('success_story/Success.html.twig ' ,['form'=>$form->createView()]);
     }
 }
