051b47e0873d2a211c957321bdf9d623d799d9d4
sabrinedachraoui <sabrine.dachraoui@esprit.tn>
2021-03-05 02:39:40 +0100

    Création views module BlogPost

--- e-nutrition-symfony/src/Form/BlogPostType.php
+++ e-nutrition-symfony/src/Form/BlogPostType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\BlogPost;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,11 @@ class BlogPostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+
             ->add('title')
-            ->add('body')
-            ->add('date')
-        ;
+            ->add('body', TextareaType::class, [
+                'attr' => ['class' => 'form-control'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
