1d8b1e1c6e121d1d00cc42f074f05bae14bff454
ALADIN <gannarala@gmail.com>
2021-03-26 23:24:51 +0100

    -tag utilisateur

--- e-nutrition-symfony/src/Entity/TagUtilisateur.php
+++ e-nutrition-symfony/src/Entity/TagUtilisateur.php
@@ -10,5 +10,20 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class TagUtilisateur extends Tag
 {
+    /**
+     * @ORM\ManyToOne(targetEntity=Utilisateur::class, inversedBy="tagUtilisateur")
+     */
+    private $utilisateur;
 
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
 }
