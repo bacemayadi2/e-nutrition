a72cdc6d50feca5dde8216f9e29a08a44c37ffe4
Sirine-bks <sirinebouksila@gmail.com>
2021-04-18 13:55:08 +0200

    ficheGui

---
+++ e-nutrition-java/src/e/nutrition/tests/FicheConsultationgui.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e.nutrition.tests;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Admin
+ */
+public class FicheConsultationgui extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/AddFicheConsultation.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Ajouter une fiche");
+      primaryStage.setScene(scene);
+        primaryStage.show();
+}
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
